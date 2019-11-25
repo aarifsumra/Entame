7f8a8b2c85a70735ffc5bc79188646f108cc8e02
Aarif Sumra <aarif.sumra@monstar-lab.com>
2019-11-25 14:05:59 +0900

    Initial Commit

---
+++ EntaMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EntaMe
+//
+//  Created by Sumra Aarif on 2019/11/25.
+//  Copyright © 2019 Sumra Aarif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
